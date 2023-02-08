e5308c55b83dfc50dbc6977e6323f713d0d6a332
Bonus156 <Bonus.by@mail.ru>
2023-02-09 00:15:31 +0300

    feat: add applying presets

---
+++ src/models/types.ts
@@ -0,0 +1,21 @@
+interface Preset {
+    presetName: string;
+    presetNameRu: string;
+    presetValues: number[];
+}
+
+interface SLink {
+    soundName: string;
+    soundSrc: string;
+}
+
+export interface Mood {
+    categoryName: string;
+    mood: string;
+    moodRu: string;
+    description: string;
+    descriptionRu: string;
+    presets: Preset[];
+    soundsDirect: string[];
+    soundsLinks: SLink[];
+}
