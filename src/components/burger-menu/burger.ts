d51bbf7c14454bfd33b8a80869c7645bbf2d6cef
rockmonolit <rockmonolit@mail.ru>
2023-02-10 17:11:11 +0300

    refactor: add typescript features

--- src/components/burger-menu/burger.ts
+++ src/components/burger-menu/burger.ts
@@ -1,29 +1,31 @@
-const line1 = document.querySelector('.line1');
-const line2 = document.querySelector('.line2');
-const line3 = document.querySelector('.line3');
-const burgerContainer = document.querySelector('.burger-container');
-const burgerModal = document.querySelector('.burger-modal');
+import { getNullCheckedElement } from '../../models/utils';
 
-function animateBurger() {
-    if (!burgerContainer?.classList.contains('clicked')) {
-        burgerContainer?.classList.add('clicked');
-        line1?.classList.add('rotate-45', 'translate-y-2.5');
-        line2?.classList.add('opacity-0');
-        line3?.classList.add('-rotate-45', '-translate-y-2.5');
-        if (burgerModal?.classList.contains('-right-96')) {
-            burgerModal?.classList.remove('-right-96');
-            burgerModal?.classList.add('right-2');
+const line1 = getNullCheckedElement(document, '.line1');
+const line2 = getNullCheckedElement(document, '.line2');
+const line3 = getNullCheckedElement(document, '.line3');
+const burgerContainer = getNullCheckedElement(document, '.burger-container');
+const burgerModal = getNullCheckedElement(document, '.burger-modal');
+
+function animateBurger(): void {
+    if (!burgerContainer.classList.contains('clicked')) {
+        burgerContainer.classList.add('clicked');
+        line1.classList.add('rotate-45', 'translate-y-2.5');
+        line2.classList.add('opacity-0');
+        line3.classList.add('-rotate-45', '-translate-y-2.5');
+        if (burgerModal.classList.contains('-right-96')) {
+            burgerModal.classList.remove('-right-96');
+            burgerModal.classList.add('right-2');
         }
-    } else if (burgerContainer?.classList.contains('clicked')) {
-        burgerContainer?.classList.remove('clicked');
-        line1?.classList.remove('rotate-45', 'translate-y-2.5');
-        line2?.classList.remove('opacity-0');
-        line3?.classList.remove('-rotate-45', '-translate-y-2.5');
-        if (burgerModal?.classList.contains('right-2')) {
-            burgerModal?.classList.remove('right-2');
-            burgerModal?.classList.add('-right-96');
+    } else if (burgerContainer.classList.contains('clicked')) {
+        burgerContainer.classList.remove('clicked');
+        line1.classList.remove('rotate-45', 'translate-y-2.5');
+        line2.classList.remove('opacity-0');
+        line3.classList.remove('-rotate-45', '-translate-y-2.5');
+        if (burgerModal.classList.contains('right-2')) {
+            burgerModal.classList.remove('right-2');
+            burgerModal.classList.add('-right-96');
         }
     }
 }
 
-burgerContainer?.addEventListener('click', animateBurger);
+burgerContainer.addEventListener('click', animateBurger);
--- src/models/utils.ts
+++ src/models/utils.ts
@@ -1,5 +1,23 @@
+import { DomClassManipulation } from './enums';
+
 function getRandomDigit(max: number) {
     return Math.floor(Math.random() * max);
+}
+
+export function getNullCheckedElement(parent: Element | Document, selector: string): Element {
+    const el = parent.querySelector(selector);
+    if (!el) {
+        throw new Error(`${selector} is not an element.`);
+    }
+    return el;
+}
+
+export function addRemoveDomClass(element: Element, classToManipulate: string, action: string): void {
+    if (action === DomClassManipulation.add) {
+        element.classList.add(classToManipulate);
+    } else if (action === DomClassManipulation.remove) {
+        element.classList.remove(classToManipulate);
+    }
 }
 
 export default getRandomDigit;
