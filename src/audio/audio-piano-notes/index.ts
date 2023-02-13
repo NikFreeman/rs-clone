e7ca800618bef10d89e3cdb527c4c78d5256abca
Bonus156 <Bonus.by@mail.ru>
2023-02-14 00:44:19 +0300

    feat: start add sound for piano keys

--- src/audio/audio-piano-notes/index.ts
+++ src/audio/audio-piano-notes/index.ts
@@ -2,17 +2,17 @@ import a0 from './01_A0.mp3';
 import a_0 from './02_A#0.mp3';
 import b0 from './03_B0.mp3';
 import c1 from './04_C1.mp3';
-import c_1 from './05_C#1.mp3';
+import c_1 from './05_C_1.mp3';
 import d1 from './06_D1.mp3';
-import d_1 from './07_D#1.mp3';
+import d_1 from './07_D_1.mp3';
 import e1 from './08_E1.mp3';
 import f1 from './09_F1.mp3';
 import f_1 from './10_F#1.mp3';
 import g1 from './11_G1.mp3';
-import g_1 from './12_G1.mp3';
+import g_1 from './12_G#1.mp3';
 import a1 from './13_A1.mp3';
 import a_1 from './14_A#1.mp3';
-import b1 from './15_B2.mp3';
+import b1 from './15_B1.mp3';
 import c2 from './16_C2.mp3';
 import c_2 from './17_C#2.mp3';
 import d2 from './18_D2.mp3';
@@ -21,10 +21,34 @@ import e2 from './20_E2.mp3';
 import f2 from './21_F2.mp3';
 import f_2 from './22_F#2.mp3';
 import g2 from './23_G2.mp3';
-import g_2 from './24_G2.mp3';
+import g_2 from './24_G#2.mp3';
 import a2 from './25_A2.mp3';
 import a_2 from './26_A#2.mp3';
 import b2 from './27_B2.mp3';
+import c3 from './28_C3.mp3';
+import c_3 from './29_C#3.mp3';
+import d3 from './30_D3.mp3';
+import d_3 from './31_D#3.mp3';
+import e3 from './32_E3.mp3';
+import f3 from './33_F3.mp3';
+import f_3 from './34_F#3.mp3';
+import g3 from './35_G3.mp3';
+import g_3 from './36_G#3.mp3';
+import a3 from './37_A3.mp3';
+import a_3 from './38_A#3.mp3';
+import b3 from './39_B3.mp3';
+import c4 from './40_C4.mp3';
+import c_4 from './41_C#4.mp3';
+import d4 from './42_D4.mp3';
+import d_4 from './43_D#4.mp3';
+import e4 from './44_E4.mp3';
+import f4 from './45_F4.mp3';
+import f_4 from './46_F#4.mp3';
+import g4 from './47_G4.mp3';
+import g_4 from './48_G#4.mp3';
+import a4 from './49_A4.mp3';
+import a_4 from './50_A#4.mp3';
+import b4 from './51_B4.mp3';
 
 const notes: string[] = [
     a0,
@@ -54,6 +78,30 @@ const notes: string[] = [
     a2,
     a_2,
     b2,
+    c3,
+    c_3,
+    d3,
+    d_3,
+    e3,
+    f3,
+    f_3,
+    g3,
+    g_3,
+    a3,
+    a_3,
+    b3,
+    c4,
+    c_4,
+    d4,
+    d_4,
+    e4,
+    f4,
+    f_4,
+    g4,
+    g_4,
+    a4,
+    a_4,
+    b4,
 ];
 
 export default notes;
--- src/components/piano/piano.ts
+++ src/components/piano/piano.ts
@@ -1,4 +1,6 @@
 import { getNullCheckedElement, addRemoveDomClass } from '../../models/utils';
+import notes from '../../audio/audio-piano-notes/index';
+import SoundPlayer from '../player';
 
 const piano = getNullCheckedElement(document, '.piano') as HTMLDivElement;
 
@@ -8,17 +10,38 @@ pianoButton.addEventListener('click', () => addRemoveDomClass(piano, 'hidden', '
 
 // drag and drop
 piano.onmousedown = function takePiano(event: MouseEvent) {
-    const volumeRange = piano.querySelector('.piano-volume');
-    if (event.target === volumeRange) {
+    const pianoFrame = document.querySelector('.piano');
+    if (event.target !== pianoFrame && event.target !== pianoFrame?.firstElementChild) {
         return;
     }
     addRemoveDomClass(piano, 'cursor-grabbing', 'add');
-    const shiftX = event.clientX - piano.getBoundingClientRect().left;
+    let shiftX = event.clientX - piano.getBoundingClientRect().left;
     const shiftY = event.clientY - piano.getBoundingClientRect().top;
 
     function moveAt(pageX: number, pageY: number) {
+        const isRightEdge =
+            piano.getBoundingClientRect().left + piano.offsetWidth > document.documentElement.clientWidth;
+        const docHeight = Math.max(
+            document.body.scrollHeight,
+            document.documentElement.scrollHeight,
+            document.body.offsetHeight,
+            document.documentElement.offsetHeight,
+            document.body.clientHeight,
+            document.documentElement.clientHeight
+        );
+        const isBottomEdge = pageY - shiftY + piano.offsetHeight >= docHeight;
+        if (isRightEdge) {
+            shiftX = piano.offsetWidth - (document.documentElement.clientWidth - pageX);
+        }
         piano.style.left = `${pageX - shiftX}px`;
-        piano.style.top = `${pageY - shiftY}px`;
+        if (piano.getBoundingClientRect().left < 0) {
+            piano.style.left = '0px';
+            shiftX = pageX;
+        }
+        piano.style.top = isBottomEdge ? `${docHeight - piano.offsetHeight}px` : `${pageY - shiftY}px`;
+        if (piano.getBoundingClientRect().top + window.pageYOffset < 0) {
+            piano.style.top = '0px';
+        }
     }
 
     moveAt(event.pageX, event.pageY);
@@ -29,13 +52,34 @@ piano.onmousedown = function takePiano(event: MouseEvent) {
 
     document.addEventListener('mousemove', onMouseMove);
 
-    piano.onmouseup = function upMouse() {
+    document.onmouseup = function upMouse() {
         addRemoveDomClass(piano, 'cursor-grabbing', 'remove');
         document.removeEventListener('mousemove', onMouseMove);
-        piano.onmouseup = null;
+        document.onmouseup = null;
     };
 };
 
 piano.ondragstart = function startDrag() {
     return false;
 };
+
+const pianoKeys = document.querySelectorAll('.key');
+const pianoNotes = new SoundPlayer(notes.slice(3, 28));
+pianoNotes.loadAll();
+const notesArr = pianoNotes.getHowl();
+notesArr.forEach((note) => note.loop(false));
+
+pianoKeys.forEach((key, index) => {
+    key.addEventListener('pointerdown', () => {
+        key.classList.add('pressed');
+        notesArr[index].play();
+    });
+    key.addEventListener('pointerout', () => {
+        key.classList.remove('pressed');
+        notesArr[index].stop();
+    });
+    key.addEventListener('pointerup', () => {
+        key.classList.remove('pressed');
+        notesArr[index].stop();
+    });
+});
